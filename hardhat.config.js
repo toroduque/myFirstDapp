4798788d9da3d61f116494e69fd9292cdcd3f8a1
Daniel Toro <toro.duque@gmail.com>
2021-12-09 21:26:29 +1100

    SAdded configuration to deploy to Rinkeby testnest

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,17 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const { SIGNER_PRIVATE_KEY, INFURA_PROJECT_ID } = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [SIGNER_PRIVATE_KEY],
+    },
+  },
 };
