5d5eccfa2848b12273ece826b59b63f08be4b538
Daniel Toro <toro.duque@gmail.com>
2021-12-12 21:50:06 +1100

    Added unit tests and fixed bugs

--- contracts/ToroArt.sol
+++ contracts/ToroArt.sol
@@ -5,11 +5,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "./Base64.sol";
 import "./ToroArtDNA.sol";
 
 contract ToroArt is ERC721, ERC721Enumerable, ToroArtDNA {
     using Counters for Counters.Counter;
+    using Strings for uint256;
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
@@ -28,6 +31,7 @@ contract ToroArt is ERC721, ERC721Enumerable, ToroArtDNA {
             msg.sender
         );
         _safeMint(msg.sender, currentId);
+        _idCounter.increment();
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -95,8 +99,8 @@ contract ToroArt is ERC721, ERC721Enumerable, ToroArtDNA {
         string memory jsonURI = Base64.encode(
             abi.encodePacked(
                 '{ "name": "ToroArt #',
-                tokenId,
-                '", "description": "ToroArt are randomized Avataars. This is just for learning purposes, relax", "image": " ',
+                tokenId.toString(),
+                '", "description": "ToroArt are randomized Avataars. This is just for learning purposes, relax", "image": "',
                 image,
                 '"}'
             )
---
+++ test/ToroArt.js
@@ -0,0 +1,67 @@
+const { expect } = require("chai");
+
+describe("ToroArt Contract", () => {
+  // The first token generated starts with id 0
+  const firstTokenId = 0;
+
+  const setup = async ({ maxSupply = 10000 }) => {
+    const [owner] = await ethers.getSigners();
+    const ToroArt = await ethers.getContractFactory("ToroArt");
+    const deployed = await ToroArt.deploy(maxSupply);
+
+    return {
+      owner,
+      deployed,
+    };
+  };
+
+  describe("Deployment", () => {
+    it("Set max supply to passed param", async () => {
+      const maxSupply = 4000;
+      const { deployed } = await setup({ maxSupply });
+      const returnedMaxSupply = await deployed.maxSupply();
+      expect(returnedMaxSupply).to.equal(returnedMaxSupply);
+    });
+  });
+
+  describe("Minting", () => {
+    it("Mints a new token and assigns it to owner", async () => {
+      const { owner, deployed } = await setup({});
+      await deployed.mint();
+
+      const ownerOfMinted = await deployed.ownerOf(firstTokenId);
+      expect(ownerOfMinted).to.equal(owner.address);
+    });
+
+    it("Has a minting limit", async () => {
+      const maxSupply = 2;
+      const { deployed } = await setup({ maxSupply });
+
+      // mint all
+      await Promise.all([deployed.mint(), deployed.mint()]);
+
+      // Assert the last minting
+      await expect(deployed.mint()).to.be.revertedWith("No ToroArts left :(");
+    });
+  });
+
+  describe("Token URI", () => {
+    it("returns valid metadata", async () => {
+      const { deployed } = await setup({});
+      await deployed.mint();
+      const tokenURI = await deployed.tokenURI(firstTokenId);
+      const stringifiedTokenURI = await tokenURI.toString();
+      const [, base64JSON] = stringifiedTokenURI.split(
+        "data:application/json;base64,"
+      );
+      const stringifiedMetadata = await Buffer.from(
+        base64JSON,
+        "base64"
+      ).toString("ascii");
+
+      const metadata = JSON.parse(stringifiedMetadata);
+
+      expect(metadata).to.have.all.keys(["name", "description", "image"]);
+    });
+  });
+});
